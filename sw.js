b8a705785af2e492fa4052993f0f9ef00a9eb2eb
Felipe De Andrade <felipedeandrademendonca@gmail.com>
2022-07-03 13:54:21 -0300

    PWA - Refatoração do cache

--- sw.js
+++ sw.js
@@ -15,37 +15,56 @@ self.addEventListener('install', function(event){
     return self.skipWaiting()
 })
 
-self.addEventListener('activate', e => {
-    self.clients.claim()
-})
+// self.addEventListener('activate', e => {
+//     self.clients.claim()
+// })
 
-self.addEventListener('fetch', async e => {
-    const req = e.request
-    const url = new URL(req.url)
+// self.addEventListener('fetch', async e => {
+//     const req = e.request
+//     const url = new URL(req.url)
 
-    if(url.origin === location.origin) {
-        e.respondWith(cacheFirst(req))
-    } else {
-        e.respondWith(networkAndCache(req))
-    }
-})
+//     if(url.origin === location.origin) {
+//         e.respondWith(cacheFirst(req))
+//     } else {
+//         e.respondWith(networkAndCache(req))
+//     }
+// })
 
-async function cacheFirst(req) {
-    const cache = await caches.open(cacheName)
-    const cached = await cache.match(req)
+// async function cacheFirst(req) {
+//     const cache = await caches.open(cacheName)
+//     const cached = await cache.match(req)
     
-    return cached || fetch(req)
-}
+//     return cached || fetch(req)
+// }
 
-async function networkAndCache(req) {
-    const cache = await caches.open(cacheName)
-    try {
-        const refresh = await fetch(req)
-        await cache.put(req, refresh.clone())
+// async function networkAndCache(req) {
+//     const cache = await caches.open(cacheName)
+//     try {
+//         const refresh = await fetch(req)
+//         await cache.put(req, refresh.clone())
 
-        return refresh
-    } catch(e) {
-        const cached = await cache.match(req)
-        return cached
-    }
-}
+//         return refresh
+//     } catch(e) {
+//         const cached = await cache.match(req)
+//         return cached
+//     }
+// }
+
+// const urlsToCache = ["./", "./index.html", "./manifest.webmanifest", "./index.js", "./css/style.css"];
+// self.addEventListener("install", (event) => {
+//    event.waitUntil(async () => {
+//       const cache = await caches.open("pwa-assets");
+//       return cache.addAll(urlsToCache);
+//    });
+// });
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+      caches.match(event.request)
+      .then(cachedResponse => {
+        // It can update the cache to serve updated content on the next request
+          return cachedResponse || fetch(event.request);
+      }
+    )
+   )
+ });
