02d8a8d29ab71a3730b97bb14bdf24aa925e3f18
Felipe De Andrade <felipedeandrademendonca@gmail.com>
2022-07-05 03:26:24 -0300

    Reorganização dos estilos em arquivos separados

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-window.addEventListener('load', () => {
-    registerSW()
-})
+// window.addEventListener('load', () => {
+//     registerSW()
+// })
 
-async function registerSW() {
-    if('serviceWorker' in navigator) {
-        try {
-            await navigator.serviceWorker.register('./sw.js')
-        } catch(e) {
-            console.error('SW registration failed');
-        }
-    }
-}
+// async function registerSW() {
+//     if('serviceWorker' in navigator) {
+//         try {
+//             await navigator.serviceWorker.register('./sw.js')
+//         } catch(e) {
+//             console.error('SW registration failed');
+//         }
+//     }
+// }
